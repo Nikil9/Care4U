9a5c512d0ef7a5c9154ff5833a32d135c4acb979
Nikil9 <saisainikil@gmail.com>
2020-04-04 00:23:32 +0530

    1 file added

---
+++ news.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class news extends Component {
+  render() {
+    return (
+      <div>
+     The story of how the novel coronavirus emerged in Wuhan, China, has produced a nasty propaganda battle between the United States and China. The two sides have traded some of the sharpest charges made between two nations since the Soviet Union in 1985 falsely accused the CIA of manufacturing AIDS.
+
+U.S. intelligence officials don’t think the pandemic was caused by deliberate wrongdoing. The outbreak that has now swept the world instead began with a simpler story, albeit one with tragic consequences: The prime suspect is “natural” transmission from bats to humans, perhaps through unsanitary markets. But scientists don’t rule out that an accident at a research laboratory in Wuhan might have spread a deadly bat virus that had been collected for scientific study.
+      </div>
+    )
+  }
+}
+export default news;
