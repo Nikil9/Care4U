9430740556bf221ed507a112d0048ea2fde352ca
Nikil9 <saisainikil@gmail.com>
2020-04-03 23:34:50 +0530

    1 binaries uploaded

--- index.js
+++ index.js
@@ -5,25 +5,78 @@ import './style.css';
 
 class App extends Component {
   render() {
-    return (  
-  <div class="topnav">
-  <a href="#news">Care4U</a>
+    var slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      var dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1}    
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";  
+      }
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";  
+      dots[slideIndex-1].className += " active";
+}
+    return (
+    <div class="topnav">
+    <a href="#news">Care4U</a>
     <div class="topnav-right">
-    <a class="active" href="#home">Home</a>
-    <a href="#news">News</a>
-    <a href="#contact">Contact</a>
-    <a href="#about">About</a>
+      <a class="active" href="#home">Home</a>
+      <a href="#news">News</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
     </div>
 
-    <frameset rows = "10%,80%,10%">
-      <frame name = "top" src = "test.html" />
-      <frame name = "main" src = "" />
-      <frame name = "bottom" src = "" />    
-   </frameset>
+    <div class="slideshow-container">
+    <div class="mySlides fade">
+    <div class="numbertext">1 / 3</div>
+    <img src="img_nature_wide.jpg" />
+    <div class="text">Caption Text</div>
+    </div>
 
-  </div>  
+    <div class="mySlides fade">
+    <div class="numbertext">2 / 3</div>
+    <img src="img_snow_wide.jpg" />
+    <div class="text">Caption Two</div>
+    </div>
+
+    <div class="mySlides fade">
+    <div class="numbertext">3 / 3</div>
+    <img src="img_mountains_wide.jpg" />
+    <div class="text">Caption Three</div>
+    </div>
+
+
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+    </div>
+
+    <div style="text-align:center">
+    <span class="dot" onclick="currentSlide(1)"></span> 
+    <span class="dot" onclick="currentSlide(2)"></span> 
+    <span class="dot" onclick="currentSlide(3)"></span> 
+  </div>
+  </div>
     );
   }
 }
 
 render(<App />, document.getElementById('root'));
+
+
+
