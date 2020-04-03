2d510888e820cc7fd5504c0c4979071ac4c7f57d
Nikil9 <saisainikil@gmail.com>
2020-04-03 22:53:00 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,26 +4,17 @@ import Hello from './Hello';
 import './style.css';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
-
   render() {
-    return (
-      
-        <div class="topnav">
-        <a href="#news">care 4 u</a>
-        <div class="topnav-right">
-  <a class="active" href="#home">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <a href="#about">About</a>
-  </div>
-</div>
-      
+    return (  
+  <div class="topnav">
+  <a href="#news">Care4U</a>
+    <div class="topnav-right">
+    <a class="active" href="#home">Home</a>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+    </div>
+  </div>  
     );
   }
 }
