3b6506f3b98d7be7dddd6986de21c83afc7c7088
Nikil9 <saisainikil@gmail.com>
2020-04-05 00:44:34 +0530

    2 files modified

--- Test.js
+++ Test.js
@@ -5,13 +5,19 @@ import './style.css';
 class Test extends Component {
   render() {
     return (
-      <div>
         <div class="about-section">
-          <h1>About Us Page</h1>
-          <p>Some text about who we are and what we do.</p>
-          <p>Resize the browser window to see that this page is responsive by the way.</p>
+          <h1>Care4U Mission</h1>
+          <p><h3>The dangerous pandemic Corona is devastating everyone. People are suffering from lack of food, water, medicines, support and there is a need to provide essential services to our people.</h3></p>
+          <br/>
+          <hr/>
+
+          <br/>
+          <br/>
+          <div class="test">
+          <p><h2>Students, Doctors, Nurse, Employees, Volunteers, any one can help,  
+             lets work for everyone! </h2></p>
+          </div>
         </div>
-      </div>
     )
   }
 }
