452a34f3e2c5f05b85dcfda39fe38c060101c0fd
Nikil9 <saisainikil@gmail.com>
2020-04-04 19:13:29 +0530

    3 files modified

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ class App extends Component {
          <SimpleMap/> 
       </div>
       <div>
-         <Test1/> 
+         <News/> 
       </div>
     </div>
     );
--- news.js
+++ news.js
@@ -1,50 +1,42 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import Chart from 'react-google-charts'
 
 class News extends Component {
   render() {
-    function openCity(evt, cityName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-}
     return (
-     /* <div>
-     The story of how the novel coronavirus emerged in Wuhan, China, has produced a nasty propaganda battle between the United States and China. The two sides have traded some of the sharpest charges made between two nations since the Soviet Union in 1985 falsely accused the CIA of manufacturing AIDS.
+     <div>
+      <Chart
+        width={'500px'}
+        height={'300px'}
+        chartType="BubbleChart"
+        loader={<div>Loading Chart</div>}
+        data={[
+          ['ID', 'Life Expectancy', 'Fertility Rate', 'Region', 'Population'],
+          ['CAN', 80.66, 1.67, 'North America', 33739900],
+          ['DEU', 79.84, 1.36, 'Europe', 81902307],
+          ['DNK', 78.6, 1.84, 'Europe', 5523095],
+          ['EGY', 72.73, 2.78, 'Middle East', 79716203],
+          ['GBR', 80.05, 2, 'Europe', 61801570],
+          ['IRN', 72.49, 1.7, 'Middle East', 73137148],
+          ['IRQ', 68.09, 4.77, 'Middle East', 31090763],
+          ['ISR', 81.55, 2.96, 'Middle East', 7485600],
+          ['RUS', 68.6, 1.54, 'Europe', 141850000],
+          ['USA', 78.09, 2.05, 'North America', 307007000],
+        ]}
+        options={{
+          title:
+            'Correlation between people affected with corona ' +
+            'and population of some world countries',
+          hAxis: { title: 'Death Expectancy' },
+          vAxis: { title: 'Covid Rate' },
+          bubble: { textStyle: { fontSize: 11 } },
+        }}
+      rootProps={{ 'data-testid': '1' }}
+      />
 
-U.S. intelligence officials don’t think the pandemic was caused by deliberate wrongdoing. The outbreak that has now swept the world instead began with a simpler story, albeit one with tragic consequences: The prime suspect is “natural” transmission from bats to humans, perhaps through unsanitary markets. But scientists don’t rule out that an accident at a research laboratory in Wuhan might have spread a deadly bat virus that had been collected for scientific study.
-      </div>*/
-      <div>
-      <div class="tab">
-  <button class="tablinks" onmouseover="openCity(event, 'London')">London</button>
-  <button class="tablinks" onmouseover="openCity(event, 'Paris')">Paris</button>
-  <button class="tablinks" onmouseover="openCity(event, 'Tokyo')">Tokyo</button>
-</div>
-
-<div id="London" class="tabcontent">
-  <h3>London</h3>
-  <p>London is the capital city of England.</p>
-</div>
-
-<div id="Paris" class="tabcontent">
-  <h3>Paris</h3>
-  <p>Paris is the capital of France.</p>
-</div>
-
-<div id="Tokyo" class="tabcontent">
-  <h3>Tokyo</h3>
-  <p>Tokyo is the capital of Japan.</p>
-</div>
-</div>
+      
+      </div>
     )
   }
 }
